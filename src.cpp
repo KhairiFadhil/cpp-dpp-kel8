8655bba4a24a980da5a8cde0ee55ad1d3a9456f2
Kalri <53751770+KhairiFadhil@users.noreply.github.com>
2024-10-10 11:49:47 +0700

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -12,8 +12,8 @@ bool logout() {
 	return false;
 }
 
-bool login(string username, string password, const vector<Account>& accounts) {
-	for (const Account& account : accounts) {
+bool login(string username, string password, const vector<Account> accounts) {
+	for (const Account account : accounts) {
 		if (username == account.username && password == account.password) {
 			cout << "Login Berhasil!, Selamat datang " << username << endl;
 			return true;
@@ -23,8 +23,8 @@ bool login(string username, string password, const vector<Account>& accounts) {
 	return false;
 }
 
-void registerAccount(string username, string password, vector<Account>& verif) {
-	for (const Account& account : verif) {
+void registerAccount(string username, string password, vector<Account> verif) {
+	for (const Account account : verif) {
 		if (username == account.username) {
 			cout << "Registrasi Gagal, Akun sudah terdaftar." << endl;
 			return;
